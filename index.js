74317c302bcb0e723f33d4dfe152c125f6e747c9
poncho29 <meneses0828@gmail.com>
2022-07-06 20:57:47 -0500

    server creado

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("my app")
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mundo')
+});
+
+app.listen(port, () => {
+  console.log(`Server listening in port ${port}`);
+});
